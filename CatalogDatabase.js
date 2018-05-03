3a98dcf338ab08b81f2ab13b64f77b297c61254d
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-03 10:57:53 -0400

    Update CatalogDatabase.js

--- CatalogDatabase.js
+++ CatalogDatabase.js
@@ -4,6 +4,15 @@ var CatalogDatabase = {
       "name": "Placeholder",
       "description": "Placeholder desc",
       "imgName":  "Unavailible.png"
+      "tags": {
+        "Unavailible", "Placeholder"
+      }
+    },
+    "1": {
+      "name": "Recycled Waterbottle",
+      "description": "It is a water bottle that is recycled, what else is there to know?",
+      "imgName":  "Unavailible.png"
     }
   }
 };
+
