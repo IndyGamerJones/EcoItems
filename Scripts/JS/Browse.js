7ea085bb39e5673b07be005a158ede22b8fe23c0
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-03 14:53:08 -0400

    Update Browse.js

--- Scripts/JS/Browse.js
+++ Scripts/JS/Browse.js
@@ -1,1 +1,19 @@
-
+document.onload = function(e) {
+  let i = 0;
+  let cardContainer = document.getElementById("product-card-container");
+  while (true) {
+    if (CatalogDatabase[i]["name"] == "") {
+      break;  
+    }
+    let browserCard = document.createElement("div");
+    browserCard.id = "product-card";
+    
+    browserCard.innerHTML = "<div class='product-card-header'>" + CatalogDatabase[i]["name"] + "</div>" +
+      "<div class='product-card-image'><div><p>" + CatalogDatabase[i]["price"]; + "</p></div><img src=" + CatalogDatabase[i]["image"] + " alt=''>" +
+      "<div class='product-card-desc'>" + CatalogDatabase[i]["description"] + "</div>";
+    
+    cardContainer.appendElement(browserCard);
+    
+    i++;
+  }
+}
