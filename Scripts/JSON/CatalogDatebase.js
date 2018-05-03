9e63082ffa0fc8a58b8811f0a5f2932e7b251567
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-03 14:36:55 -0400

    Update CatalogDatebase.js

--- Scripts/JSON/CatalogDatebase.js
+++ Scripts/JSON/CatalogDatebase.js
@@ -1,17 +1,22 @@
 var CatalogDatabase = {
   "Items": {
-    "0": {
+    0: {
       "name": "Placeholder",
       "description": "Placeholder desc",
-      "imgName":  "Unavailible.png"
+      "imgName":  "Unavailible.png",
+      "price": 10,
       "tags": {
         "Unavailible", "Placeholder"
       }
     },
-    "1": {
+    1: {
       "name": "Recycled Waterbottle",
       "description": "It is a water bottle that is recycled, what else is there to know?",
-      "imgName":  "Unavailible.png"
+      "imgName":  "Unavailible.png",
+      "price": 15,
+      "tags": {
+        "Waterbottle", "Recycled"
+      }
     }
   }
 };
