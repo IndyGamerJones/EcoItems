5693becb07e0d82c163066fdb3b1f9caeede44eb
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-03 14:27:44 -0400

    Create CatalogDatebase.js

---
+++ Scripts/JSON/CatalogDatebase.js
@@ -0,0 +1,17 @@
+var CatalogDatabase = {
+  "Items": {
+    "0": {
+      "name": "Placeholder",
+      "description": "Placeholder desc",
+      "imgName":  "Unavailible.png"
+      "tags": {
+        "Unavailible", "Placeholder"
+      }
+    },
+    "1": {
+      "name": "Recycled Waterbottle",
+      "description": "It is a water bottle that is recycled, what else is there to know?",
+      "imgName":  "Unavailible.png"
+    }
+  }
+};
