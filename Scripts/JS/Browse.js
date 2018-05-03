772af31ed5485f4a95e01745454bbdab01c62700
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-03 18:39:04 -0400

    Update Browse.js

--- Scripts/JS/Browse.js
+++ Scripts/JS/Browse.js
@@ -2,15 +2,15 @@ window.onload = function() {
   let i = 0;
   let cardContainer = document.getElementById("product-card-container");
   while (true) {
-    if (CatalogDatabase[i]["name"] == "") {
+    if (CatalogDatabase["Items"][i]["name"] == "") {
       break;  
     }
     let browserCard = document.createElement("div");
     browserCard.id = "product-card";
     
-    browserCard.innerHTML = "<div class='product-card-header'>" + CatalogDatabase[i]["name"] + "</div>" +
-      "<div class='product-card-image'><div><p>" + CatalogDatabase[i]["price"]; + "</p></div><img src=" + CatalogDatabase[i]["image"] + " alt=''>" +
-      "<div class='product-card-desc'>" + CatalogDatabase[i]["description"] + "</div>";
+    browserCard.innerHTML = "<div class='product-card-header'>" + CatalogDatabase["Items"][i]["name"] + "</div>" +
+      "<div class='product-card-image'><div><p>" + CatalogDatabase["Items"][i]["price"]; + "</p></div><img src=" + CatalogDatabase["Items"][i]["image"] + " alt=''>" +
+      "<div class='product-card-desc'>" + CatalogDatabase["Items"][i]["description"] + "</div>";
     
     cardContainer.appendElement(browserCard);
     
