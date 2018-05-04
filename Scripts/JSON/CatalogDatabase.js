d570066c2617b013de03370b872c46ca00d9f070
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-04 14:24:20 -0400

    Update CatalogDatabase.js

--- Scripts/JSON/CatalogDatabase.js
+++ Scripts/JSON/CatalogDatabase.js
@@ -26,6 +26,60 @@ var CatalogDatabase = {
       "tags": [
         "Flag", "Finland"
       ]
+    },
+    {
+      "name": "Placeholder",
+      "description": "Placeholder desc",
+      "imgName":  "Unavailable.png",
+      "price": 10,
+      "tags": [
+        "Missing-Picture","Unavailable", "Placeholder"
+      ]
+    },
+    {
+      "name": "Recycled Waterbottle",
+      "description": "It is a water bottle that is recycled, what else is there to know?",
+      "imgName":  "Unavailable.png",
+      "price": 15,
+      "tags": [
+        "Missing-Picture", "Waterbottle", "Recycled"
+      ]
+    },
+    {
+      "name":  "Finnish Flag",
+      "description":  "A flag of Finland",
+      "imgName":  "Unavailable.png",
+      "price": 1234,
+      "tags": [
+        "Flag", "Finland"
+      ]
+    },
+    {
+      "name": "Placeholder",
+      "description": "Placeholder desc",
+      "imgName":  "Unavailable.png",
+      "price": 10,
+      "tags": [
+        "Missing-Picture","Unavailable", "Placeholder"
+      ]
+    },
+    {
+      "name": "Recycled Waterbottle",
+      "description": "It is a water bottle that is recycled, what else is there to know?",
+      "imgName":  "Unavailable.png",
+      "price": 15,
+      "tags": [
+        "Missing-Picture", "Waterbottle", "Recycled"
+      ]
+    },
+    {
+      "name":  "Finnish Flag",
+      "description":  "A flag of Finland",
+      "imgName":  "Unavailable.png",
+      "price": 1234,
+      "tags": [
+        "Flag", "Finland"
+      ]
     }
   ]
 };
