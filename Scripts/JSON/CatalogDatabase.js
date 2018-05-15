1e2c920c962bfe0def79d323b5109390416b0c5c
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-15 13:57:25 -0400

    Update CatalogDatabase.js

--- Scripts/JSON/CatalogDatabase.js
+++ Scripts/JSON/CatalogDatabase.js
@@ -1,30 +1,66 @@
 var CatalogDatabase = {
   "Items": [
     {
-      "name": "Placeholder",
-      "description": "Placeholder desc",
-      "imgName":  "Unavailable.png",
-      "price": 10,
+      "name": "Edible Water",
+      "description": "A pack of water bottles that is made from sodium alginate gel which allows the water bottle to be easily eaten along with the refreshing water",
+      "imgName":  "Edible_Water.png",
+      "price": 3,
       "tags": [
         "Missing-Picture","Unavailable", "Placeholder"
       ]
     },
     {
-      "name": "Recycled Waterbottle",
-      "description": "It is a water bottle that is recycled, what else is there to know?",
+      "name": "Recycled Water bottle",
+      "description": "A water bottle made from the oceans waste and is molded in water bottles through melting them to create a pure plastic which will be further refined into a safe and cheap plastic bottle",
       "imgName":  "Unavailable.png",
-      "price": 15,
+      "price": 0.20,
       "tags": [
-        "Missing-Picture", "Waterbottle", "Recycled"
+        "Missing-Picture", "Unavailable", "Placeholder"
       ]
     },
     {
-      "name":  "Finnish Flag",
-      "description":  "A flag of Finland",
+      "name":  "Eco-Friendly Tent",
+      "description":  "A tent made with solar panels to power outlets and is made from recycled a plastic fabric polymer blend",
       "imgName":  "Unavailable.png",
-      "price": 1234,
+      "price": 300,
       "tags": [
-        "Flag", "Finland"
+        "Tent", "Eco-friendly"
+      ]
+   },
+    {
+      "name": "Fabrix Containers",
+      "description": "A company that makes fabric out of plastic waste and takes metal and glass products and molds them into different products like silverware or containers.",
+      "imgName":  "Metal_Container.png",
+      "price": 8,
+      "tags": [
+        "Missing-Picture", "Unavailable", "Placeholder"
+      ]
+ },
+    {
+      "name": "Edible Silverware",
+      "description": "Spoons made from a blend of flours that allows them to be easily and conveniently eaten. Shaped as a 100 pack that can be flavored with sugars",
+      "imgName":  "Metal_Container.png",
+      "price": 4,
+      "tags": [
+        "Missing-Picture", "Unavailable", "Placeholder"
+      ]
+   },
+    {
+      "name": "Remade Foil",
+      "description": "Foil that is remade by recasting the foil through melting and refining which allows them to make their foil. Metal and metal pieces",
+      "imgName":  "Edible_Spoon.png",
+      "price": 2,
+      "tags": [
+        "Missing-Picture", "Unavailable", "Placeholder"
+      ]
+   },
+    {
+      "name": "Recycled Finnish Flag",
+      "description": "A Finnish Flag made from plastic fabric",
+      "imgName":  "Unavailable.png",
+      "price": 50,
+      "tags": [
+        "Missing-Picture", "Unavailable", "Placeholder"
       ]
     }
   ]
