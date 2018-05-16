fa3a10c64c6a3d9fa9fd6885a772698608f19a5c
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-16 11:05:39 -0400

    Update Browse.js

--- Scripts/JS/Browse.js
+++ Scripts/JS/Browse.js
@@ -26,8 +26,8 @@ function LoadProduct(index) {
   let panel = document.getElementById("product-panel");
   panel.setAttribute("on","true");
 
-  panel.innerHTML = "<table class='align_table' style='width: 84%;'><tr><td><img style='width:28%;height: auto;float:left;' src='../Images/Items/" + CatalogDatabase.Items[index].imgName + "'></td><td style='width: 28%;'>" +
-    CatalogDatabase.Items[index].name + "<br><br>" + CatalogDatabase.Items[index].description + "</td><td style='width:28%; float: right;'><p style='float:right;font-size:2vw;' onclick='UnloadProduct'>X</p></td></tr></table>";
+  panel.innerHTML = "<p style='float:right;margin-top: 0.5vw;margin-right: 0.5vw;font-size:2vw;' onclick='UnloadProduct'>X</p><table class='align_table' style='width: 84%;'><tr><td style='width: 47%; float:left;'><img style='width:100%;height: auto;float:left;' src='../Images/Items/" + CatalogDatabase.Items[index].imgName + "'></td><td style='width: 47%;float:right;'>" +
+    CatalogDatabase.Items[index].name + "<br><br>" + CatalogDatabase.Items[index].description + "</td></tr></table>";
 }
 function UnloadProduct() {
   let panel = document.getElementById("product-panel");
