cb51668acdb811acc94d65c6b8eeab580e2a9752
IndyGamerJones <tehcosmicsloth@gmail.com>
2018-05-16 10:55:18 -0400

    Update Browse.js

--- Scripts/JS/Browse.js
+++ Scripts/JS/Browse.js
@@ -27,7 +27,7 @@ function LoadProduct(index) {
   panel.setAttribute("on","true");
 
   panel.innerHTML = "<table class='align_table' style='width: 84%;'><tr><td><img style='width:28%;height: auto;float:left;' src='../Images/Items/" + CatalogDatabase.Items[index].imgName + "'></td><td style='width: 28%;'>" +
-    CatalogDatabase.Items[index].name + "<br><br>" + CatalogDatabase.Items[index].description + "</td><td style='width:28%; float: right;'><p style='float:right;font-size:2vw;'>X<p></td></tr></table>";
+    CatalogDatabase.Items[index].name + "<br><br>" + CatalogDatabase.Items[index].description + "</td><td style='width:28%; float: right;'><p style='float:right;font-size:2vw;' onclick='UnloadProduct'>X</p></td></tr></table>";
 }
 function UnloadProduct() {
   let panel = document.getElementById("product-panel");
